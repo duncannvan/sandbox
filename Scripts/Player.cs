4b2f5bf21269f9e2b0ddb66bba004ae0f37a9b4a
Duncan Van <vanduncann@gmail.com>
2026-02-20 20:42:28 -0600

    first person movement and enviroment

---
+++ Scripts/Player.cs
@@ -0,0 +1,62 @@
+using Godot;
+
+public partial class Player : CharacterBody3D
+{
+    private const byte SPEED = 10;
+    private const float MOUSE_SENSE = 0.002f;
+    private const float MAX_CAM_ANGEL_RAD = Mathf.Pi / 2;
+
+    private Camera3D _camera;
+    private float _cameraRotationX = 0f;
+
+    public override void _Ready()
+    {
+        SetupCamera();
+    }
+
+    public override void _UnhandledInput(InputEvent @event)
+    {
+        if (@event is InputEventMouseMotion mouseMotion)
+        {
+            HandleCameraRotation(mouseMotion);
+        }
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        HandleMovement();
+    }
+
+    private void HandleCameraRotation(InputEventMouseMotion mouseMotion)
+    {
+        // Rotate body
+        RotateY(-mouseMotion.Relative.X * MOUSE_SENSE);
+
+        // Rotate camera up/down
+        _cameraRotationX = Mathf.Clamp(_cameraRotationX - mouseMotion.Relative.Y * MOUSE_SENSE, -MAX_CAM_ANGEL_RAD, MAX_CAM_ANGEL_RAD);
+        _camera.Rotation = new Vector3(_cameraRotationX, 0, 0);
+    }
+
+    private void HandleMovement()
+    {
+        Vector2 inputDirection = Input.GetVector("move_left", "move_right", "move_forward", "move_back");
+
+        if(inputDirection == Vector2.Zero) return;
+
+        Vector3 frontBackDirection = _camera.GlobalTransform.Basis.Z * inputDirection.Y;
+        Vector3 leftRightDirection =  _camera.GlobalTransform.Basis.X * inputDirection.X;
+        Vector3 direction = (frontBackDirection + leftRightDirection).Normalized();
+        direction.Y = 0;
+
+        Velocity = direction * SPEED;
+        MoveAndSlide();
+    }
+
+    private void SetupCamera()
+    {
+        _camera = GetNode<Camera3D>("%PlayerCamera");
+        Input.MouseMode = Input.MouseModeEnum.Captured;
+    }
+
+
+}
